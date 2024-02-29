54810a1c97cefc58e5aa4aef76dc114e2e74378b
pfigithub <pampam7072@gmail.com>
2024-02-29 14:10:29 +0330

    data cleaning

--- hierarchical.py
+++ hierarchical.py
@@ -9,3 +9,15 @@ from sklearn.cluster import AgglomerativeClustering
 # reading
 pdf = pd.read_csv('yourdata')
 pdf.head(5)
+
+# data cleaning base of our data(it is car_clus)
+print ("Shape of dataset before cleaning: ", pdf.size)
+pdf[[ 'sales', 'resale', 'type', 'price', 'engine_s',
+       'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap',
+       'mpg', 'lnsales']] = pdf[['sales', 'resale', 'type', 'price', 'engine_s',
+       'horsepow', 'wheelbas', 'width', 'length', 'curb_wgt', 'fuel_cap',
+       'mpg', 'lnsales']].apply(pd.to_numeric, errors='coerce')
+pdf = pdf.dropna()
+pdf = pdf.reset_index(drop=True)
+print ("Shape of dataset after cleaning: ", pdf.size)
+pdf.head(5)
